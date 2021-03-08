2be79ded9deb5577836f9b93120b0bb2102e1faa
Arif Hasan <arif.saiket@gmail.com>
2021-03-08 13:54:43 +0600

    Initial commit

---
+++ app/Models/Upazila.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Upazila extends Model
+{
+
+    protected $fillable = ['name', 'bn_name', 'district_id'];
+
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
+
+    public function post_offices()
+    {
+        return $this->hasMany(PostOffice::class, 'upazila_id');
+    }
+
+    public function unions()
+    {
+        return $this->hasMany(Union::class, 'upazila_id');
+    }
+}
